b9050634257fd483b7d584cf8443b9e250097eea
Nathan <nathanelie.06@gmail.com>
2026-03-26 19:08:54 +0100

    feat modification dao intervention (pas fini)

--- 26emacoeur/src/main/java/bts/sio/emacoeur/database/DaoIntervention.java
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/database/DaoIntervention.java
@@ -4,12 +4,60 @@
  */
 package bts.sio.emacoeur.database;
 
+import bts.sio.emacoeur.model.Intervention;
+import bts.sio.emacoeur.model.TypeIntervention;
+import com.sun.source.tree.CatchTree;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author nathan
  */
+
 public class DaoIntervention {
     
+    Connection cnx = null;
+    static PreparedStatement requete = null;
+    static ResultSet rs = null;
     
-    
+    public static ArrayList<Intervention> getLesInterventions(Connection cnx){ 
+        
+
+        ArrayList<Intervention> desInters = new ArrayList<Intervention>();
+        try{
+
+            requete=cnx.prepareStatement(" select ");
+            
+            rs=requete.executeQuery();
+            
+            while(rs.next()){
+                
+                Intervention uneInter = new Intervention();
+                uneInter.setId(rs.getInt("i_id"));
+                
+                Date sqlDateIntervention = rs.getDate("i_dateInter");
+                uneInter.setDateIntervention(sqlDateIntervention.toLocalDate());
+                
+                TypeIntervention typeInter = new TypeIntervention();
+                typeInter.setId(rs.getInt("ty_id"));
+                typeInter.setLibelle(rs.getString("ty_libelle"));
+                
+                uneInter.setTypeintervention(typeInter);
+                
+                desInters.add(uneInter);
+                
+            }
+        }catch(Exception e){
+            
+            e.printStackTrace();
+            System.out.println("Erreur lors de l'etablissement de la connexion");
+            
+        }
+        return desInters;
+    }
 }
