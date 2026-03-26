5cb733da7794955ea230eca7be728538771dce0a
Nathan <nathanelie.06@gmail.com>
2026-03-26 09:21:32 +0100

    Modification TestTypeIntervention, pour relations One To many et Many to one

--- 26emacoeur/src/main/java/bts/sio/emacoeur/tests/TestTypeIntervention.java
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/tests/TestTypeIntervention.java
@@ -4,7 +4,9 @@
  */
 package bts.sio.emacoeur.tests;
 
+import bts.sio.emacoeur.model.Intervention;
 import bts.sio.emacoeur.model.TypeIntervention;
+import java.time.LocalDate;
 
 /**
  *
@@ -17,7 +19,22 @@ public class TestTypeIntervention {
          typeintervention1.setId(1);
          typeintervention1.setLibelle("Incendie");
          
+         Intervention inter1 = new Intervention(1, LocalDate.of(2026, 03, 22));
+         
+         
+         TypeIntervention typeintervention2 = new TypeIntervention(5, "Malaise");
+         
+         Intervention i1 = new Intervention(66, LocalDate.of(2020, 05, 18));
+         Intervention i2 = new Intervention(90, LocalDate.of(2019,06,30));
+         
+         typeintervention2.addUneIntervention(i1);
+         typeintervention2.addUneIntervention(i2);
+         
          System.out.println("Le type d'intervention comportant l'id " + typeintervention1.getId() + " a comme libelle " + typeintervention1.getLibelle());
-    
-}
+         
+         for (Intervention i : typeintervention2.getDesInterventions()){
+             System.out.println("* "+i.getId()+" "+i.getDateIntervention());
+             
+         }
+    }
 }
