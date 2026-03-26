0468bf65ec94fd79c3b0948eb5834a044cfe5362
Jules Fossey <julesfossey76@gmail.com>
2026-03-26 11:26:40 +0100

    debut de réparation du projet suppression des classes volontaire et professionnel

--- 26emacoeur/src/main/java/bts/sio/emacoeur/model/Profession.java
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/model/Profession.java
@@ -3,65 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package bts.sio.emacoeur.model;
-import java.util.ArrayList;
-
 
 /**
  *
  * @author ts1sio
  */
 public class Profession {
-    
     private int id;
     private String libelle;
     private String ville;
-    private ArrayList<Volontaire> desVolontaires;
-
-    public Profession() {
-    }
-
-    public Profession(int id, String libelle, String ville) {
-        this.id = id;
-        this.libelle = libelle;
-        this.ville = ville;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public String getVille() {
-        return ville;
-    }
-
-    public ArrayList<Volontaire> getDesVolontaires() {
-        return desVolontaires;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
-
-    public void setVille(String ville) {
-        this.ville = ville;
-    }
-
-    public void setDesVolontaires(ArrayList<Volontaire> desVolontaires) {
-        this.desVolontaires = desVolontaires;
-    }
-
-    public void addUnVolontaire(Volontaire unVolontaire) {
-        if (desVolontaires == null) {
-            desVolontaires = new ArrayList<Volontaire>();
-        }
-        desVolontaires.add(unVolontaire);
-    }
+    
+    
 }
--- 26emacoeur/src/main/java/bts/sio/emacoeur/tests/TestProfession.java
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/tests/TestProfession.java
@@ -4,38 +4,10 @@
  */
 package bts.sio.emacoeur.tests;
 
-import bts.sio.emacoeur.model.Profession;
-import bts.sio.emacoeur.model.Volontaire;
-import java.time.LocalDate;
-
 /**
  *
  * @author ts1sio
  */
 public class TestProfession {
     
-    public static void main(String[] args) {
-
-        Profession profession1 = new Profession(1, "Infirmier", "Paris");
-
-        Volontaire vol1 = new Volontaire(1, "Dupont", "Jean", LocalDate.of(1985, 6, 15), 101);
-        vol1.setProfession(profession1);
-
-        Volontaire vol2 = new Volontaire(2, "Martin", "Claire", LocalDate.of(1990, 9, 20), 102);
-        vol2.setProfession(profession1);
-
-        profession1.addUnVolontaire(vol1);
-        profession1.addUnVolontaire(vol2);
-
-        System.out.println("La profession n " + profession1.getId() 
-                + " est " + profession1.getLibelle() 
-                + " a " + profession1.getVille()
-                + " possede ces volontaires :");
-
-        for (Volontaire v : profession1.getDesVolontaires()) {
-            System.out.println(" Volontaire : " + v.getPrenom() 
-                    + " " + v.getNom() 
-                    + ", bip n " + v.getNumeroBip());
-        }
-    }
 }
