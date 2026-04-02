6076172cdd07cbce58670f2e493530eaded39fdb
Nathan <nathanelie.06@gmail.com>
2026-04-02 11:17:51 +0200

    feat creation methode getEnginById dans dao-engin

--- 26emacoeur/src/main/java/bts/sio/emacoeur/database/DaoEngin.java
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/database/DaoEngin.java
@@ -8,6 +8,7 @@ import bts.sio.emacoeur.model.Engin;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -43,4 +44,30 @@ public class DaoEngin {
         return desEngins;
         
     } 
+    
+    public static Engin getEnginById(Connection cnx, int idEngin){
+        
+        Engin unEngin = new Engin();
+        
+        try{
+            
+            requete=cnx.prepareStatement("SELECT eng_id as e_id, eng_type as e_type \n " +
+                            "FROM engin; \n");
+            
+            requete.setInt(1,idEngin);
+            rs=requete.executeQuery();
+            
+            if( rs.next() ){
+                unEngin.setId(rs.getInt("e_id"));
+                unEngin.setType(rs.getString("e_type"));
+            }
+            
+        }
+        catch( SQLException e ){
+            
+            e.printStackTrace();
+        }
+        return unEngin;
+        
+    }
 }
