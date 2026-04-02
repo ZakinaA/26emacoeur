c9b1971ffd53cd6e2fe0ff1e1ad500cf838a3fce
Nathan <nathanelie.06@gmail.com>
2026-04-02 10:53:45 +0200

    feat ajout methode getLesEngins

---
+++ 26emacoeur/src/main/java/bts/sio/emacoeur/database/DaoEngin.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bts.sio.emacoeur.database;
+
+import bts.sio.emacoeur.model.Engin;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nathan
+ */
+public class DaoEngin {
+    
+    static PreparedStatement requete = null;
+    static ResultSet rs = null;
+    
+    public static ArrayList<Engin> getLesEngins(Connection cnx) {
+        
+        
+        
+        return null;
+        
+        
+    } 
+}
